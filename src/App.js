ab855fc9c77149676be89fa1d60e1d09ae5c5532
Edgar <edgar.nurullin@onetwotrip.com>
2018-02-13 15:14:06 +0300

    refactoring)

--- src/App.js
+++ src/App.js
@@ -1,96 +1,110 @@
-import React, { Component } from 'react';
-import ToDo from './components/ToDo';
+import React, {Component} from 'react';
+import ToDo, { TASK_STATUSES } from './components/ToDo';
 import './css/styles.css';
 import Emoji from 'react-emoji-render';
 import Cookies from 'universal-cookie';
 
-
 class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			task: '',
-			list: ['Check it out in GitHub', 'Made with ❤️ by Toni Codina', 'Try making a new task above 👆', 'Build your own!', 'This site uses 🍪 to keep track of your tasks'],
-			done: []
-		}
-		this.handleClick = this.handleClick.bind(this);
-		this.removeTodo = this.removeTodo.bind(this);
-		this.completeTodo = this.completeTodo.bind(this);
-	}
-	componentWillMount() {
-		const cookies = new Cookies();
-		if (cookies.get('todo') !== undefined) {
-			this.setState({
-				list: cookies.get('todo')
-			});
-		}
-		if (cookies.get('done') !== undefined) {
-			this.setState({
-				done: cookies.get('done')
-			});
-		}
-	}
-	onChange = (event) => {
-		this.setState({ task: event.target.value });
-	}
-	removeTodo(name, type){
-		if (type === 1) {
-			const cookies = new Cookies();
-			var array = this.state.list;
-			var index = array.indexOf(name);
-			array.splice(index, 1);
-			this.setState({
-				list: array
-			});
-			cookies.set('todo', JSON.stringify(array), { path: '/'});
-		} else {
-			const cookies = new Cookies();
-			var array = this.state.done;
-			var index = array.indexOf(name);
-			array.splice(index, 1);
-			this.setState({
-				done: array
-			});
-			cookies.set('done', JSON.stringify(array), { path: '/'});
-		}
-	}
-	completeTodo(name) {
-		const cookies = new Cookies();
-		this.removeTodo(name, 1);
-		var join = this.state.done.slice();
-		join.push(name);
-		this.setState({
-    		done: join
-  		});
-		cookies.set('done', JSON.stringify(join), { path: '/'});
-	}
-	handleClick() {
-	if(this.state.task !== '') {
-	this.setState({
-		task: '',
-    	list: [...this.state.list, this.state.task]
-   }, () => {
-		const cookies = new Cookies();
-		cookies.set('todo', JSON.stringify(this.state.list), { path: '/'});
-		cookies.set('done', JSON.stringify(this.state.done), { path: '/'});
-	});
-	}
-	}
-	handleKey = (event) => {
-  	if(event.key == 'Enter'){
-			this.handleClick();
-		}
-	}
-	render() {
-		return (
-		<div className="header">
-		<h1>My tasks<Emoji text="✍" /></h1>
-		<input placeholder="Ex: Write a new blog post" maxLength={80} value={this.state.task} type='text' onKeyPress={this.handleKey} task={this.state.task} onChange={this.onChange}/>
-		<button onClick={this.handleClick}>+</button>
-		<ToDo tasks={this.state.list} done={this.state.done} remove={this.removeTodo} complete={this.completeTodo}/>
-		</div>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: '',
+            list: ['Check it out in GitHub', 'Made with ❤️ by Toni Codina', 'Try making a new task above 👆', 'Build your own!', 'This site uses 🍪 to keep track of your tasks'],
+            done: []
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
+        this.completeTodo = this.completeTodo.bind(this);
+    }
+
+    componentWillMount() {
+        const cookies = new Cookies();
+        if (cookies.get('todo') !== undefined) {
+            this.setState({
+                list: cookies.get('todo')
+            });
+        }
+        if (cookies.get('done') !== undefined) {
+            this.setState({
+                done: cookies.get('done')
+            });
+        }
+    }
+
+    onChange = (event) => {
+        this.setState({task: event.target.value});
+    };
+
+    removeTodo(name, type) {
+        switch (type) {
+            case TASK_STATUSES.TO_DO: {
+                const cookies = new Cookies();
+                var array = this.state.list;
+                var index = array.indexOf(name);
+                array.splice(index, 1);
+                this.setState({
+                    list: array
+                });
+                cookies.set('todo', JSON.stringify(array), {path: '/'});
+            } break;
+            case TASK_STATUSES.DONE: {
+                const cookies = new Cookies();
+                var array = this.state.done;
+                var index = array.indexOf(name);
+                array.splice(index, 1);
+                this.setState({
+                    done: array
+                });
+                cookies.set('done', JSON.stringify(array), {path: '/'});
+            } break;
+            default: {
+                // nothing
+            } break;
+        }
+    }
+
+    completeTodo(name) {
+        const cookies = new Cookies();
+        this.removeTodo(name, TASK_STATUSES.TO_DO);
+        var join = this.state.done.slice();
+        join.push(name);
+        this.setState({
+            done: join
+        });
+        cookies.set('done', JSON.stringify(join), {path: '/'});
+    }
+
+    handleClick() {
+        if (this.state.task !== '') {
+            this.setState({
+                task: '',
+                list: [...this.state.list, this.state.task]
+            }, () => {
+                const cookies = new Cookies();
+                cookies.set('todo', JSON.stringify(this.state.list), {path: '/'});
+                cookies.set('done', JSON.stringify(this.state.done), {path: '/'});
+            });
+        }
+    }
+
+    handleKey = (event) => {
+        if (event.key == 'Enter') {
+            this.handleClick();
+        }
+    }
+
+    render() {
+        return (
+            <div className="header">
+                <h1>My tasks<Emoji text="✍"/></h1>
+                <input placeholder="Ex: Write a new blog post" maxLength={80} value={this.state.task} type='text'
+                       onKeyPress={this.handleKey} task={this.state.task} onChange={this.onChange}/>
+                <button onClick={this.handleClick}>+</button>
+                <ToDo tasks={this.state.list} done={this.state.done} remove={this.removeTodo}
+                      complete={this.completeTodo}/>
+            </div>
+        );
+    }
 }
 
 export default Home;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,42 +1,50 @@
-import React, { Component } from 'react';
-import { Check } from 'react-feather';
-import { X } from 'react-feather';
+import React, {Component} from 'react';
+import {Check} from 'react-feather';
+import {X} from 'react-feather';
 import Emoji from 'react-emoji-render';
 
+export const TASK_STATUSES = {
+    TO_DO: 'TO_DO',
+    DONE: 'DONE'
+};
+
 class ToDo extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleChange = this.handleChange.bind(this);
-		this.handleDone = this.handleDone.bind(this);
-  	}
-	handleChange(name, type) {
-		this.props.remove(name, type);
-	}
-	handleDone(name) {
-		this.props.complete(name);
-	}
-	render() {
-		var passTarget = this;
-		const rlist = this.props.tasks.map((list) =>
-		<div key={list.toString()}>
-			<li className="todo">{list}</li>
-			<Check className="check" color="white" size={17} onClick={() => this.handleDone(list)} />
-			<X className="cross" color="white" size={17} onClick={() => this.handleChange(list, 1)} />
-		</div>
-		);
-		const dlist = this.props.done.map((done) =>
-		<div key={done.toString()}>
-			<li className="done">{done}</li>
-			<X className="cross" color="white" size={17} onClick={() => this.handleChange(done, 2)} />
-		</div>
-		);
-		return (
-			<div>
-			<ul>{rlist}</ul>
-			{this.props.done.length ? <h1>Completed tasks <Emoji text="👏" /></h1> : ''}
-			<ul>{dlist}</ul>
-			</div>
-			);
-	}
+    constructor(props) {
+        super(props);
+    }
+
+    handleChange(name, type) {
+        this.props.remove(name, type);
+    }
+
+    handleDone(name) {
+        this.props.complete(name);
+    }
+
+    render() {
+        const rlist = this.props.tasks.map((list, idx) =>
+            <div key={idx}>
+                <li className="todo">{list}</li>
+                <Check className="check" color="white" size={17} onClick={() => this.handleDone(list)}/>
+                <X className="cross" color="white" size={17} onClick={this.handleChange.bind(this, list, TASK_STATUSES.TO_DO)}/>
+            </div>
+        );
+
+        const dlist = this.props.done.map((done, idx) =>
+            <div key={idx}>
+                <li className="done">{done}</li>
+                <X className="cross" color="white" size={17} onClick={this.handleChange.bind(this, done, TASK_STATUSES.DONE)}/>
+            </div>
+        );
+
+        return (
+            <div>
+                <ul>{rlist}</ul>
+                {this.props.done.length ? <h1>Completed tasks <Emoji text="👏"/></h1> : ''}
+                <ul>{dlist}</ul>
+            </div>
+        );
+    }
 }
+
 export default ToDo;
